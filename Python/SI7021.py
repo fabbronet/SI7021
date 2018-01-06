2e9281159f5ac0931673ce95aa00862d342c6280
Sam Povilus <povilus@povil.us>
2018-01-05 18:13:28 -0700

    telling python that this is unicode so the degree symbol works

--- Python/SI7021.py
+++ Python/SI7021.py
@@ -1,4 +1,5 @@
 # Distributed with a free-will license.
+# -*- coding: utf-8 -*-, 
 # Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
 # SI7021
 # This code is designed to work with the SI7021_I2CS I2C Mini Module available from ControlEverything.com.
